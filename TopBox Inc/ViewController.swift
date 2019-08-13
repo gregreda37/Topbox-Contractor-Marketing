db52cf4a093897f2b3d527421ec3fd0aa9ad2c98
Gregory Reda <gregreda37@gmail.com>
2019-08-13 14:08:26 -0400

    Initial Commit

---
+++ TopBox Inc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopBox Inc
+//
+//  Created by Gregory Reda on 8/13/19.
+//  Copyright © 2019 TopBox Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TopBox IncTests/TopBox_IncTests.swift
@@ -0,0 +1,34 @@
+//
+//  TopBox_IncTests.swift
+//  TopBox IncTests
+//
+//  Created by Gregory Reda on 8/13/19.
+//  Copyright © 2019 TopBox Inc. All rights reserved.
+//
+
+import XCTest
+@testable import TopBox_Inc
+
+class TopBox_IncTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
